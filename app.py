5e0a848797e510344db47e75e05221c0b9cce729
Sarvesh Raktawan <sarvesh.raktawan1@gmail.com>
2023-12-16 07:15:11 +0530

    futf

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-
+import pickle
 app = Flask(__name__)
 
 
@@ -16,7 +16,11 @@ def predict():
         y = request.form.get('y')
         z = request.form.get('z')
         color_encoded = request.form.get('color_encoded')
-        return render_template('sucess.html',data = {'Diamond Price':69897})
+        # Load the model from the file
+        with open('model.pkl', 'rb') as model_file:
+            mlmodel = pickle.load(model_file)
+        pred = mlmodel.predict([[float(carat),float(table),float(x),float(y),float(z),float(color_encoded)]])
+        return render_template('success.html',data = {'Diamond Price':round(pred[0],2)})
     else:
         return render_template('predict.html')
 
